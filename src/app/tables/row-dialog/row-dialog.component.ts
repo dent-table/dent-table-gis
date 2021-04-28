89f3720fc8e56b3ea3d253844be96a5c0a6645e7
vincios <v.nunziata16@gmail.com>
2021-04-28 19:44:43 +0200

    Added validated_by filed into tables

--- src/app/tables/row-dialog/row-dialog.component.ts
+++ src/app/tables/row-dialog/row-dialog.component.ts
@@ -1,4 +1,11 @@
-import {AfterViewInit, ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 import {DatabaseService} from '../../providers/database.service';
 import {TableDefinition} from '../../model/model';
 import {
@@ -14,6 +21,9 @@ import * as moment from 'moment';
 import {Utils} from '../../commons/Utils';
 import * as _ from 'lodash-es';
 import {LoggerService} from '../../providers/logger.service';
+import {Observable, Subscription, timer} from "rxjs";
+import {map, switchMap, tap} from "rxjs/operators";
+import {ShowOnDirtyErrorStateMatcher} from "@angular/material/core";
 
 export interface DialogData {
   tableId: any;
@@ -27,7 +37,7 @@ export interface DialogData {
   templateUrl: './row-dialog.component.html',
   styleUrls: ['./row-dialog.component.scss'],
 })
-export class RowDialogComponent implements OnInit, AfterViewInit {
+export class RowDialogComponent implements OnInit, AfterViewInit, OnDestroy {
   logTag = RowDialogComponent.name;
 
   tableDefinition: TableDefinition;
@@ -38,6 +48,32 @@ export class RowDialogComponent implements OnInit, AfterViewInit {
 
   dialogType: string;
   specialCases;
+
+  /** Show mat-error when invalid control is dirty, touched, or submitted.
+   * https://stackoverflow.com/questions/51456487/why-mat-error-not-get-displayed-inside-mat-form-field-in-angular-material-6-with
+   */
+  dirtyMatcher = new ShowOnDirtyErrorStateMatcher();
+
+  /* caching validation observables subscription */
+  statusChangeObservable: Subscription;
+  validationUserNameObservable: Subscription;
+  currentValidationId: number;
+  currentValidationUser: string;
+  searchUserPending: boolean;
+
+
+  validationUserAsyncValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return timer(300).pipe(switchMap((index, value) => {
+        return this.databaseService.getValidationUserName(Number.parseInt(control.value, 10)).pipe(
+          map(res => {
+            return res ? null : {id_exists: false};
+          }),
+          tap(() => setTimeout(() => this.cdr.detectChanges(), 0))
+        );
+      }));
+    };
+  }
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
@@ -60,6 +96,11 @@ export class RowDialogComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.tableDefinition = this.data.tableDefinition;
+
+    if (this.data.element.validation_name) {
+      this.currentValidationUser = this.data.element.validation_name;
+      this.currentValidationId = this.data.element.validation_id;
+    }
 
     if (!this.tableDefinition) {
       this.databaseService.getTableDefinition(this.data.tableId).toPromise().then((values => {
@@ -88,17 +129,24 @@ export class RowDialogComponent implements OnInit, AfterViewInit {
     }
   }
 
+  ngOnDestroy(): void {
+    this.logger.debug(this.logTag, "Destroying observables.. ")
+    if (this.validationUserNameObservable) { this.validationUserNameObservable.unsubscribe(); }
+    if (this.statusChangeObservable) { this.statusChangeObservable.unsubscribe(); }
+  }
+
   typeOf = Utils.typeof;
 
   createFormGroup() {
-    const group = {};
+    const disablingControls = {}; // controls in this group will be disabled if validated_by field is not valid
+
     let elementSlotNumber = this.data.element ? this.data.element['slot_number'] : '';
     // check if slot number is one of special cases
     elementSlotNumber = Utils.specialCase(elementSlotNumber, this.data.tableId) === false
       ? elementSlotNumber
       : Utils.specialCase(elementSlotNumber, this.data.tableId);
 
-    group['slot_number'] = new FormControl(elementSlotNumber);
+    disablingControls['slot_number'] = this.fb.control(elementSlotNumber);
 
     for (const column of this.tableDefinition.columnsDefinition) {
       const validators = [];
@@ -115,15 +163,43 @@ export class RowDialogComponent implements OnInit, AfterViewInit {
         validators.push(Validators.required);
       }
 
-      group[column.name] = new FormControl(currentValue || '', validators);
+      // special columns needs something else
+      if (column.type.special) { //TODO: add this to dent-table
+        if (column.name === 'validated_by') {
+          // validated_by field needs that the inserted id exists into database
+          asyncValidators.push(this.validationUserAsyncValidator());
+        }
+      }
+
+      // group[column.name] = new FormControl(currentValue || '',
+      //   {validators: validators, asyncValidators: asyncValidators, updateOn: "change"});
+
+      disablingControls[column.name] = this.fb.control(currentValue || '', //TODO: add this to dent-table
+        {validators: validators, asyncValidators: asyncValidators, updateOn: "change"});
     }
-    this.formGroup = new FormGroup(group);
+
+    // we have to remove validated_by field from the control group
+    const validatedByControl: AbstractControl = disablingControls['validated_by'];
+    delete disablingControls['validated_by'];
+
+    this.statusChangeObservable = validatedByControl.statusChanges.subscribe(status => this.enableFormIfValid(status));
+
+    this.formGroup = this.fb.group({ //TODO: add this to dent-table
+      validated_by: validatedByControl,
+      disablingControls: this.fb.group(disablingControls)
+    });
+
+    if (!this.currentValidationUser) {
+      this.formGroup.get(['disablingControls']).disable();
+    }
     this.cdr.detectChanges();
   }
 
   onInsert() {
     if (this.formGroup.valid) {
       const values = this.formGroup.value;
+      _.merge(values, values.disablingControls);
+      delete values.disablingControls;
 
       this.databaseService.insertRow(this.data.tableId, values).toPromise().then((result) => {
         this.dialogRef.close(result);
@@ -178,5 +254,46 @@ export class RowDialogComponent implements OnInit, AfterViewInit {
 
      return controlsStatus;
    }*/
+
+  enableFormIfValid(status: string) {
+    this.searchUserPending = status === 'PENDING';
+
+    if (status === 'INVALID') {
+      this.disableForm();
+    } else if (status === 'VALID') {
+      this.enableForm();
+    }
+  }
+
+  private enableForm() {
+    const form = this.formGroup.get('disablingControls');
+    const formValue = Number.parseInt(this.formGroup.get('validated_by').value);
+
+    if (formValue !== this.currentValidationId) {
+      if (this.validationUserNameObservable) {
+        this.validationUserNameObservable.unsubscribe();
+      }
+      this.currentValidationId = formValue;
+      this.validationUserNameObservable = this.databaseService.getValidationUserName(this.currentValidationId).subscribe(
+        (value => {
+            if (value) {
+              this.currentValidationUser = value;
+              form.enable();
+              this.cdr.detectChanges();
+            }
+          }
+        )
+      );
+    }
+  }
+
+  disableForm(): void {
+    const form = this.formGroup.get('disablingControls');
+
+    if (!this.validationUserNameObservable?.closed) { this.validationUserNameObservable?.unsubscribe(); }
+    this.currentValidationId = null;
+    this.currentValidationUser = null;
+    form.disable();
+  }
 
 }
